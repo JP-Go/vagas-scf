8fd4bdb0e647446211ec2f3a0d4a1238c8911dd2
João Pedro <jpedro1580@gmail.com>
2023-06-14 18:02:15 +0000

    Update access count on resource reads

--- teste1.js
+++ teste1.js
@@ -1,26 +1,28 @@
 var data = require("./fakeData");
+var accessController = require("./accessControl");
 
 const getUser = (req, res, next) => {
-
   let name = req.query.name;
 
-  const maybeUser = data.find(u => 
-    u.name.toLowerCase().includes(name.toLowerCase()))
+  const maybeUser = data.find((u) =>
+    u.name.toLowerCase().includes(name.toLowerCase())
+  );
 
-  if (!maybeUser)
-    return res.status(404).json({ message: "not found" })
+  if (!maybeUser) return res.status(404).json({ message: "not found" });
 
-  return res.status(200).json({ user: maybeUser })
-
+  accessController.registerVisitToUserProfile(maybeUser.name);
+  return res.status(200).json({ user: maybeUser });
 };
 
 const getUsers = (req, res, next) => {
-
+  // Register a visit for each user since all of them are read here
+  data.forEach((user) => {
+    accessController.registerVisitToUserProfile(user.name);
+  });
   res.status(200).json({ users: data });
-
 };
 
 module.exports = {
   getUser,
-  getUsers
+  getUsers,
 };
