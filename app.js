454cc15f31e8e41967ef858312c0fddd35a64f15
João Pedro <jpedro1580@gmail.com>
2023-06-14 16:56:21 +0000

    Formatting and remove body-parser since it's already included with express since April 2019

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
 var app = express();
 
 var teste1 = require("./teste1");
@@ -8,16 +7,14 @@ var teste3 = require("./teste3");
 var teste4 = require("./teste4");
 var teste5 = require("./teste5");
 
-
-app.set('view engine', 'jade');
+app.set("view engine", "jade");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
+app.get("/", function (req, res) {
   res.send(`get user/ </br>
   get users/ </br>
   post users/ </br>
@@ -28,13 +25,12 @@ app.get('/', function(req, res) {
 
 app.get("/user", teste1.getUser);
 app.get("/users", teste1.getUsers);
-app.post("/users", teste2)
-app.delete("/users", teste3)
-app.put("/users", teste4)
+app.post("/users", teste2);
+app.delete("/users", teste3);
+app.put("/users", teste4);
 app.get("/users/access", teste5);
 
-
 const port = 3000;
-app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
+app.listen(port, function () {
+  console.log("Express server listening on port " + port);
 });
